e6d0f307a3ed56170f410253b1f2985877317d1c
magdalenahuget <magdalena.huget@gmail.com>
2023-04-10 13:02:44 +0200

    Refactor

--- src/main/java/org/example/drive/HDDDrive.java
+++ src/main/java/org/example/drive/HDDDrive.java
@@ -1,33 +1,33 @@
-package org.example.drive;
-
-import org.example.File;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class HDDDrive implements Drive {
-    List<File> files = new ArrayList<>();
-
-    @Override
-    public void addFile(File file) {
-        files.add(file);
-    }
-
-    @Override
-    public void listFiles() {
-        files.forEach(System.out::println);
-
-//        for(File file: files) {
-//            System.out.println(file);
-//        }
-    }
-
-    @Override
-    public File findFile(String name) {
-        public File finFile = files.stream();
-        files.stream()
-                .filter(file -> file.getName().equals(name))
-                .findFirst();
-        return foundFile.orElseThrow();
-    }
-}
+//package org.example.drive;
+//
+//import org.example.File;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+////public class HDDDrive implements Drive {
+////    List<File> files = new ArrayList<>();
+////
+////    @Override
+////    public void addFile(File file) {
+////        files.add(file);
+////    }
+////
+////    @Override
+////    public void listFiles() {
+////        files.forEach(System.out::println);
+//
+////        for(File file: files) {
+////            System.out.println(file);
+////        }
+//    }
+//
+////    @Override
+////    public File findFile(String name) {
+////        File foundFile = (File) files.stream();
+////        files.stream()
+////                .filter(file -> file.getName().equals(name))
+////                .findFirst();
+////        return foundFile.orElseThrow();
+////    }
+//}
